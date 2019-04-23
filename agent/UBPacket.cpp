918c778f1223af3736effdcfd933ce61b7e5e7ca
jleu64 <43893485+jleu64@users.noreply.github.com>
2019-04-23 12:38:27 -0400

    Update UBPacket.cpp

--- agent/UBPacket.cpp
+++ agent/UBPacket.cpp
@@ -14,82 +14,57 @@ QByteArray UBPacket::packetize(void) {
 }
 QByteArray UBPacket::packetizePos(QGeoCoordinate currentpos, QGeoCoordinate previewpos) // Designed packets (In progress)
 {
-  QByteArray lat = QByteArray::number(currentpos.latitude(), 'g', 10); // (10 total digits ( 2 integers and 8 decimal points after) equals 33 bytes)
-  lat = lat.rightJustified(10, '0', true);
+  QByteArray lat = QByteArray::number(currentpos.latitude(), 'g', 11); // (10 total digits ( 2 integers and 8 decimal points after) equals 33 bytes)
+  lat = lat.rightJustified(11, '0', true);
 
 
-  QByteArray lon = QByteArray::number(currentpos.longitude(), 'g', 10);
-  lon = lon.rightJustified(10, '0', true);
+  QByteArray lon = QByteArray::number(currentpos.longitude(), 'g', 11);
+  lon = lon.rightJustified(11, '0', true);
 
   double bearing = previewpos.azimuthTo(currentpos); // calculates drone bearing as integer
   QByteArray bearingarray = QByteArray::number(bearing, 'g', 10);
   bearingarray = bearingarray.rightJustified(3, '0', true); // bearing relative north
-//  qInfo()<<lat + lon + bearingarray << endl;
+  qInfo()<<lat + lon + bearingarray << endl;
   return lat + lon + bearingarray;
 }
-void UBPacket::depacketize(const QByteArray& packet) {
+
+ void UBPacket::depacketize(const QByteArray& packet) {
     m_srcID = *((quint8*)(packet.mid(0, sizeof(quint8)).data()));
     m_desID = *((quint8*)(packet.mid(0 + sizeof(quint8), sizeof(quint8)).data()));
 
     m_payload = packet.mid(0 + sizeof(quint8) + sizeof(quint8));
 }
-UBPacket depacketizepos(const QByteArray &packet){ // returns packet
-  QByteArray header = packet.mid(0,4);
-   double m_lat = packet.mid(0+4, 10).toDouble();
-   double m_lon =  packet.mid(0+4+10, 10).toDouble();
-   double m_bearing = packet.mid(0+4+10+3, 10).toDouble();
+
+void UBPacket::depacketizePos(){ //
+  m_lat = *((double*)(m_payload.mid(0+4, 10).data()));
+  m_lon =  *((double*)(m_payload.mid(0+4+10, 10).data()));
+  m_bearing = *((double*)(m_payload.mid(0+4+10+3, 10).data()));
+
 }
 
- UBPacket processPacket(const QByteArray &packet)
+void UBPacket::depacketizeNoFly(){
+
+}
+
+UBPacket UBPacket::processPacket(const QByteArray &instruction)
 {
+  UBPacket pkt;
   QByteArray opt1("0000"); // first 4 numbers of received packets
   QByteArray opt2("0001");
-  QByteArray opt3("0010");
-  QByteArray opt4("0100");
-  QByteArray opt5("1000");
 
-  QByteArray header = packet.mid(0,4);
+  QByteArray header = instruction.mid(0,4);
+  pkt.setPayload(instruction);
   if (header == opt1){
-
+    pkt.depacketizeNoFly();
 
   }
   else if (header == opt2) {
-
-
-  }
-  else if (header == opt3) {
-
-
-  }
-  else if (header == opt4) {
-
-
-  }
-  else if (header == opt5) {
-
+    pkt.depacketizePos();
 
   }
   else {
     qInfo() << "Header not supported";
 
   }
+  return pkt;
 }
-/*QByteArray UBPacket::processPacket(const QByteArray & packet) // the paceket received from server to instruct the drone
-{
-
-  QByteArray header = packet.mid(0,4);
-  switch (header)
-    case "0000" // read first 4 numbers of our received packet
-        m_mission_state = STATE_IDLE;
-        break;
-    case "0001"
-        break;
-    case "0010"
-        break;
-    case "0100"
-        break;
-    case "1000"
-        break;
-    default:
-        break;
- }*/
