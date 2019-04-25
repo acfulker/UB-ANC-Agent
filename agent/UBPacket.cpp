c4d53cd5d91fe947afdf717095feda68ac77b58f
jleu64 <43893485+jleu64@users.noreply.github.com>
2019-04-24 20:41:00 -0400

    Update UBPacket.cpp

--- agent/UBPacket.cpp
+++ agent/UBPacket.cpp
@@ -12,23 +12,27 @@ QByteArray UBPacket::packetize(void) {
 
     return src + des + m_payload;
 }
-QByteArray UBPacket::packetizePos(QGeoCoordinate currentpos, QGeoCoordinate previewpos) // Designed packets (In progress)
-{
-  QByteArray lat = QByteArray::number(currentpos.latitude(), 'g', 11); // (10 total digits ( 2 integers and 8 decimal points after) equals 33 bytes)
-  lat = lat.rightJustified(11, '0', true);
+QByteArray UBPacket::packetizePos(QGeoCoordinate currentpos, QGeoCoordinate previewpos){ // Designed packets
+  QByteArray header ("1");
+  QByteArray lat = QByteArray::number(currentpos.latitude(), 'g', 8); // (10 total digits ( 2 integers and 8 decimal points after) equals 20 total bytes)
+  lat = lat.rightJustified(8, '0', true);
 
 
-  QByteArray lon = QByteArray::number(currentpos.longitude(), 'g', 11);
-  lon = lon.rightJustified(11, '0', true);
+  QByteArray lon = QByteArray::number(currentpos.longitude(), 'g', 8);
+  lon = lon.rightJustified(8, '0', true);
 
   double bearing = previewpos.azimuthTo(currentpos); // calculates drone bearing as integer
-  QByteArray bearingarray = QByteArray::number(bearing, 'g', 10);
+  QByteArray bearingarray = QByteArray::number(bearing, 'g', 3);
   bearingarray = bearingarray.rightJustified(3, '0', true); // bearing relative north
-  qInfo()<<lat + lon + bearingarray << endl;
-  return lat + lon + bearingarray;
+  qInfo()<<header + lat + lon + bearingarray << endl;
+  return header + lat + lon + bearingarray;
 }
 
- void UBPacket::depacketize(const QByteArray& packet) {
+/*QByteArray UBPacket::packetizeNoFly() { //in progress..
+  QByteArray NoFlyZone
+}*/
+
+void UBPacket::depacketize(const QByteArray& packet) {
     m_srcID = *((quint8*)(packet.mid(0, sizeof(quint8)).data()));
     m_desID = *((quint8*)(packet.mid(0 + sizeof(quint8), sizeof(quint8)).data()));
 
@@ -36,35 +40,31 @@ QByteArray UBPacket::packetizePos(QGeoCoordinate currentpos, QGeoCoordinate prev
 }
 
 void UBPacket::depacketizePos(){ //
-  m_lat = *((double*)(m_payload.mid(0+4, 10).data()));
-  m_lon =  *((double*)(m_payload.mid(0+4+10, 10).data()));
-  m_bearing = *((double*)(m_payload.mid(0+4+10+3, 10).data()));
-
+  m_lat = m_payload.mid(0+1, 25).toDouble();
+  m_lon =  m_payload.mid(0+1+25, 25).toDouble();
 }
 
 void UBPacket::depacketizeNoFly(){
-
+  m_NoFlyZone = *((quint8*)(m_payload.mid(0+1, 1).data()));
 }
 
-UBPacket UBPacket::processPacket(const QByteArray &instruction)
+void UBPacket::processPacket(const QByteArray &instruction)
 {
-  UBPacket pkt;
-  QByteArray opt1("0000"); // first 4 numbers of received packets
-  QByteArray opt2("0001");
+  QByteArray opt1("0");
+  QByteArray opt2("1");
 
-  QByteArray header = instruction.mid(0,4);
-  pkt.setPayload(instruction);
-  if (header == opt1){
-    pkt.depacketizeNoFly();
-
+  QByteArray header = instruction.mid(0,1);
+  this->setPayload(instruction);
+  if (header == "0"){
+    this->depacketizeNoFly();
+    qInfo()<<"Decoding No Fly"<<endl;
   }
-  else if (header == opt2) {
-    pkt.depacketizePos();
-
+  else if (header == "1") {
+    this->depacketizePos();
+    qInfo()<<"Decoding Position"<<endl;
   }
   else {
     qInfo() << "Header not supported";
 
   }
-  return pkt;
 }
