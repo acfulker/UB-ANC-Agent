7601e4c25580286597a6dddcd4d6f7e79371306b
jleu64 <43893485+jleu64@users.noreply.github.com>
2019-04-26 23:59:59 -0400

    Update UBSerial.cpp

--- agent/UBSerial.cpp
+++ agent/UBSerial.cpp
@@ -1,1 +1,15 @@
+#include "UBAgent.h"
+#include "UBNetwork.h"
+#include "UBPacket.h"
+#include "UBConfig.h"
+#include <QTimer>
+#include <QCommandLineParser>
+#include "Vehicle.h"
+#include "TCPLink.h"
+#include "QGCApplication.h"
+#include  <QSerialPort>
 
+ 
+SerialConfiguration* serial = new SerialConfiguration("Serial Port");
+serial->setBaud(BAUD_RATE);
+serial->setPortName(SERIAL_PORT);
