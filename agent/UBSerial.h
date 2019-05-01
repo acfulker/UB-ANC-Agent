1f513bbfc8bb891983c910873f177901a58a1900
jleu64 <43893485+jleu64@users.noreply.github.com>
2019-05-01 17:34:27 -0400

    Update UBSerial.h

--- agent/UBSerial.h
+++ agent/UBSerial.h
@@ -3,6 +3,7 @@
 #define UBSERIAL_H
 
 #include <QSerialPort>
+#include "UBPacket.h"
 
 class UBSerial : public QSerialPort
 {
@@ -10,15 +11,15 @@ class UBSerial : public QSerialPort
 public:
     explicit UBSerial(QSerialPort *parent = 0);
 
-    signals:
-            void dataReady(UBPacket data);
+signals:
+    void dataReadySerial(UBPacket packet);
 
 public slots:
-            void setID(quint8 id) {m_id = id;}
+    void setID(quint8 id) {m_id = id;}
     void sendData(quint8 desID, QByteArray data);
 
 protected slots:
-            void dataReadyEvent();
+    void dataReadyEvent();
 
 private:
     quint8 m_id;
