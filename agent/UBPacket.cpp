a0b64636f94f62711fbfc3ed8e1b9f28c020bb6d
jleu64 <43893485+jleu64@users.noreply.github.com>
2019-05-08 16:19:06 -0400

    Update UBPacket.cpp

--- agent/UBPacket.cpp
+++ agent/UBPacket.cpp
@@ -12,26 +12,17 @@ QByteArray UBPacket::packetize(void) {
 
     return src + des + m_payload;
 }
+
 QByteArray UBPacket::packetizePos(QGeoCoordinate currentpos, QGeoCoordinate previewpos){ // Designed packets
-  QByteArray header ("1");
-  QByteArray lat = QByteArray::number(currentpos.latitude(), 'g', 8); // (10 total digits ( 2 integers and 8 decimal points after) equals 20 total bytes)
-  lat = lat.rightJustified(8, '0', true);
-
-
-  QByteArray lon = QByteArray::number(currentpos.longitude(), 'g', 8);
-  lon = lon.rightJustified(8, '0', true);
-
+  QByteArray lat = QByteArray::number(currentpos.latitude(), 'g', 11); // (10 total digits ( 2 integers and 8 decimal points after) equals 20 total bytes)
+  lat = lat.rightJustified(11, '0', true);
+  QByteArray lon = QByteArray::number(currentpos.longitude(), 'g', 11);
+  lon = lon.rightJustified(11, '0', true);
   double bearing = previewpos.azimuthTo(currentpos); // calculates drone bearing as integer
   QByteArray bearingarray = QByteArray::number(bearing, 'g', 3);
   bearingarray = bearingarray.rightJustified(3, '0', true); // bearing relative north
-  //qInfo()<<header + lat + lon + bearingarray << endl;
-  return header + lat + lon + bearingarray;
-}
-
-QByteArray UBPacket::packetizeNoFly(quint8 NoFly) { //in progress..
-  QByteArray header ("0");
-  QByteArray NoFlyZone(((char*)(&NoFly)), sizeof(quint8));
-  return header + NoFlyZone; 
+  //qInfo()<<lat + lon + bearingarray << endl;
+  return lat + lon + bearingarray;
 }
 
 void UBPacket::depacketize(const QByteArray& packet) {
@@ -41,9 +32,10 @@ void UBPacket::depacketize(const QByteArray& packet) {
     m_payload = packet.mid(0 + sizeof(quint8) + sizeof(quint8));
 }
 
-void UBPacket::depacketizePos(){ //
-  m_lat = m_payload.mid(0+1, 25).toDouble();
-  m_lon =  m_payload.mid(0+1+25, 25).toDouble();
+void UBPacket::depacketizePos(){
+  m_lat = m_payload.mid(0+1, 11).toDouble();
+  m_lon =  m_payload.mid(0+1+11, 11).toDouble();
+  qInfo()<<"m_lat:"<<m_lat<<"m_lon:"<<m_lon<<endl;
   m_type = POS;
 }
 
@@ -72,4 +64,3 @@ void UBPacket::processPacket(const QByteArray &instruction)
 
   }
 }
-
